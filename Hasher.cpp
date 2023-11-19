c81d53290ef2401ec1c0d83e0f2008d3c0161d19
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-11-19 13:56:48 +0600

    move implementations to .cpp

--- Hasher.cpp
+++ Hasher.cpp
@@ -1,3 +1,143 @@
 #include "Hasher.h"
 
 // Functions implementation go here
+#include <algorithm> // for std::transform()
+#include <fstream>
+#include <iomanip> // for input/output manipulation (toHex)
+
+#include <bitset> // for debugging
+
+void AbstractHasher::toHex() {
+    // Transform binary hash string into hexadecimal
+#ifdef DEBUG
+    std::cout << "Binary string:" << std::endl;
+    for (unsigned char c: binary_hash) {
+        std::bitset<8> bits(c);
+        std::cout << bits << ' ';
+    }
+    std::cout << std::endl;
+#endif
+    std::stringstream ss;
+    for (char byte: binary_hash) {
+        /*
+         * (int)(unsigned char)(byte) - transform a signed char to unsigned and then into an integer 0..255
+         * std::setw(2) - set length of a hex digit to 2 slots
+         * std::setfill('0') - fill slots with 0 if empty up to 2 slots
+         * std::hex - interpret input as a hexadecimal string
+         */
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int) (unsigned char) (byte);
+    }
+    hash = ss.str();
+    std::transform(hash.begin(), hash.end(), hash.begin(), ::toupper);
+}
+
+void AbstractHasher::calculateHash() {
+    EVP_MD_CTX *digest_context = EVP_MD_CTX_new();
+    if (digest_context == nullptr) {
+        throw std::runtime_error("Failed to create EVP_MD_CTX");
+    }
+//        Initialize digest operation
+    initializeDigest(digest_context);
+
+//        Process chunks of data and update hash_array
+    char buffer[4096];
+    while (file.read(buffer, sizeof(buffer)) || file.gcount()) {
+        if (EVP_DigestUpdate(digest_context, buffer, file.gcount()) != 1) {
+            EVP_MD_CTX_free(digest_context);
+            throw std::runtime_error("Failed to update digest");
+        }
+    }
+
+//        Finish processing hash_array
+    unsigned char hash_array[EVP_MAX_MD_SIZE];
+    unsigned int lengthOfHash = 0;
+    if (EVP_DigestFinal_ex(digest_context, hash_array, &lengthOfHash) != 1) {
+//            Free memory
+        EVP_MD_CTX_free(digest_context);
+        throw std::runtime_error("Failed to finalize digest");
+    }
+
+    EVP_MD_CTX_free(digest_context);
+
+//        Turn to string and generate hexadecimal hash_array
+    binary_hash = std::string(reinterpret_cast<const char *>(hash_array), lengthOfHash);
+    toHex();
+};
+
+void AbstractHasher::checkHash() {
+//        Make sure hash is already generated
+    if (binary_hash.empty()) {
+        calculateHash();
+    }
+}
+
+std::string AbstractHasher::getHash() {
+    checkHash();
+    return hash;
+}
+
+std::string AbstractHasher::getBinaryHash() {
+    checkHash();
+    return binary_hash;
+}
+
+void AbstractHasher::showBinaryHash() {
+    checkHash();
+    for (unsigned char c: binary_hash) {
+        std::bitset<8> bits(c);
+        std::cout << bits << ' ';
+    }
+    std::cout << std::endl;
+}
+
+void AbstractHasher::showHash() {
+    std::cout << getHash() << std::endl;
+}
+
+bool AbstractHasher::validate(const std::string &input) {
+    checkHash();
+    return (hash == input);
+}
+
+void HasherSHA1::initializeDigest(EVP_MD_CTX *digest_context) {
+    if (EVP_DigestInit_ex(digest_context, EVP_sha1(), nullptr) != 1) {
+        EVP_MD_CTX_free(digest_context);
+        throw std::runtime_error("Failed to initialize digest context");
+    }
+}
+
+
+void HasherSHA256::initializeDigest(EVP_MD_CTX *digest_context) {
+    if (EVP_DigestInit_ex(digest_context, EVP_sha256(), nullptr) != 1) {
+        EVP_MD_CTX_free(digest_context);
+        throw std::runtime_error("Failed to initialize digest context");
+    }
+}
+
+void HasherSHA3_256::initializeDigest(EVP_MD_CTX *digest_context) {
+    if (EVP_DigestInit_ex(digest_context, EVP_sha3_256(), nullptr) != 1) {
+        EVP_MD_CTX_free(digest_context);
+        throw std::runtime_error("Failed to initialize digest context");
+    }
+}
+
+void HasherSHA3_512::initializeDigest(EVP_MD_CTX *digest_context) {
+    if (EVP_DigestInit_ex(digest_context, EVP_sha3_512(), nullptr) != 1) {
+        EVP_MD_CTX_free(digest_context);
+        throw std::runtime_error("Failed to initialize digest context");
+    }
+}
+
+void HasherMD5::initializeDigest(EVP_MD_CTX *digest_context) {
+    if (EVP_DigestInit_ex(digest_context, EVP_md5(), nullptr) != 1) {
+        EVP_MD_CTX_free(digest_context);
+        throw std::runtime_error("Failed to initialize digest context");
+    }
+}
+
+void HasherMD4::initializeDigest(EVP_MD_CTX *digest_context) {
+    if (EVP_DigestInit_ex(digest_context, EVP_md4(), nullptr) != 1) {
+        EVP_MD_CTX_free(digest_context);
+        throw std::runtime_error("Failed to initialize digest context");
+    }
+}
--- Hasher.h
+++ Hasher.h
@@ -2,15 +2,11 @@
 #define CHECKSUMS_HASHER_H
 
 #include <string>
+#include <fstream>
 #include <iostream>
 #include <openssl/evp.h>
-
-#include <iomanip> // for input/output manipulation (toHex)
-#include <fstream>
 #include <utility> // for std::move()
-#include <algorithm> // for std::transform()
 
-#include <bitset> // for debugging
 
 class AbstractHasher {
 protected:
@@ -19,64 +15,11 @@ protected:
     std::string filename;
     std::ifstream file;
 
-    void toHex() {
-        // Transform binary hash string into hexadecimal
-#ifdef DEBUG
-        std::cout << "Binary string:" << std::endl;
-        for (unsigned char c: binary_hash) {
-            std::bitset<8> bits(c);
-            std::cout << bits << ' ';
-        }
-        std::cout << std::endl;
-#endif
-        std::stringstream ss;
-        for (char byte: binary_hash) {
-            /*
-             * (int)(unsigned char)(byte) - transform a signed char to unsigned and then into an integer 0..255
-             * std::setw(2) - set length of a hex digit to 2 slots
-             * std::setfill('0') - fill slots with 0 if empty up to 2 slots
-             * std::hex - interpret input as a hexadecimal string
-             */
-            ss << std::hex << std::setw(2) << std::setfill('0') << (int) (unsigned char) (byte);
-        }
-        hash = ss.str();
-        std::transform(hash.begin(), hash.end(), hash.begin(), ::toupper);
-    }
+    void toHex();
 
     virtual void initializeDigest(EVP_MD_CTX *digest_context) = 0;
 
-    void calculateHash() {
-        EVP_MD_CTX *digest_context = EVP_MD_CTX_new();
-        if (digest_context == nullptr) {
-            throw std::runtime_error("Failed to create EVP_MD_CTX");
-        }
-//        Initialize digest operation
-        initializeDigest(digest_context);
-
-//        Process chunks of data and update hash_array
-        char buffer[4096];
-        while (file.read(buffer, sizeof(buffer)) || file.gcount()) {
-            if (EVP_DigestUpdate(digest_context, buffer, file.gcount()) != 1) {
-                EVP_MD_CTX_free(digest_context);
-                throw std::runtime_error("Failed to update digest");
-            }
-        }
-
-//        Finish processing hash_array
-        unsigned char hash_array[EVP_MAX_MD_SIZE];
-        unsigned int lengthOfHash = 0;
-        if (EVP_DigestFinal_ex(digest_context, hash_array, &lengthOfHash) != 1) {
-//            Free memory
-            EVP_MD_CTX_free(digest_context);
-            throw std::runtime_error("Failed to finalize digest");
-        }
-
-        EVP_MD_CTX_free(digest_context);
-
-//        Turn to string and generate hexadecimal hash_array
-        binary_hash = std::string(reinterpret_cast<const char *>(hash_array), lengthOfHash);
-        toHex();
-    };
+    void calculateHash();
 
 public:
     explicit AbstractHasher(std::string filename) : filename(std::move(filename)),
@@ -99,51 +42,23 @@ public:
 
     virtual ~AbstractHasher() = default;
 
-    void checkHash() {
-//        Make sure hash is already generated
-        if (binary_hash.empty()) {
-            calculateHash();
-        }
-    }
+    void checkHash();
 
-    std::string getHash() {
-        checkHash();
-        return hash;
-    }
+    std::string getHash();
 
-    std::string getBinaryHash() {
-        checkHash();
-        return binary_hash;
-    }
+    std::string getBinaryHash();
 
-    void showBinaryHash() {
-        checkHash();
-        for (unsigned char c: binary_hash) {
-            std::bitset<8> bits(c);
-            std::cout << bits << ' ';
-        }
-        std::cout << std::endl;
-    }
+    void showBinaryHash();
 
-    void showHash() {
-        std::cout << getHash() << std::endl;
-    }
+    void showHash();
 
-    bool validate(const std::string &input) {
-        checkHash();
-        return (hash == input);
-    }
+    bool validate(const std::string &input);
 
 };
 
 class HasherSHA1 : public AbstractHasher {
 private:
-    void initializeDigest(EVP_MD_CTX *digest_context) override {
-        if (EVP_DigestInit_ex(digest_context, EVP_sha1(), nullptr) != 1) {
-            EVP_MD_CTX_free(digest_context);
-            throw std::runtime_error("Failed to initialize digest context");
-        }
-    }
+    void initializeDigest(EVP_MD_CTX *digest_context) override;
 
 public:
     using AbstractHasher::AbstractHasher;
@@ -151,12 +66,7 @@ public:
 
 class HasherSHA256 : public AbstractHasher {
 private:
-    void initializeDigest(EVP_MD_CTX *digest_context) override {
-        if (EVP_DigestInit_ex(digest_context, EVP_sha256(), nullptr) != 1) {
-            EVP_MD_CTX_free(digest_context);
-            throw std::runtime_error("Failed to initialize digest context");
-        }
-    }
+    void initializeDigest(EVP_MD_CTX *digest_context) override;
 
 public:
     using AbstractHasher::AbstractHasher;
@@ -164,12 +74,7 @@ public:
 
 class HasherSHA3_256 : public AbstractHasher {
 private:
-    void initializeDigest(EVP_MD_CTX *digest_context) override {
-        if (EVP_DigestInit_ex(digest_context, EVP_sha3_256(), nullptr) != 1) {
-            EVP_MD_CTX_free(digest_context);
-            throw std::runtime_error("Failed to initialize digest context");
-        }
-    }
+    void initializeDigest(EVP_MD_CTX *digest_context) override;
 
 public:
     using AbstractHasher::AbstractHasher;
@@ -177,12 +82,7 @@ public:
 
 class HasherSHA3_512 : public AbstractHasher {
 private:
-    void initializeDigest(EVP_MD_CTX *digest_context) override {
-        if (EVP_DigestInit_ex(digest_context, EVP_sha3_512(), nullptr) != 1) {
-            EVP_MD_CTX_free(digest_context);
-            throw std::runtime_error("Failed to initialize digest context");
-        }
-    }
+    void initializeDigest(EVP_MD_CTX *digest_context) override;
 
 public:
     using AbstractHasher::AbstractHasher;
@@ -190,12 +90,7 @@ public:
 
 class HasherMD5 : public AbstractHasher {
 private:
-    void initializeDigest(EVP_MD_CTX *digest_context) override {
-        if (EVP_DigestInit_ex(digest_context, EVP_md5(), nullptr) != 1) {
-            EVP_MD_CTX_free(digest_context);
-            throw std::runtime_error("Failed to initialize digest context");
-        }
-    }
+    void initializeDigest(EVP_MD_CTX *digest_context) override;
 
 public:
     using AbstractHasher::AbstractHasher;
@@ -204,12 +99,7 @@ public:
 
 class HasherMD4 : public AbstractHasher {
 private:
-    void initializeDigest(EVP_MD_CTX *digest_context) override {
-        if (EVP_DigestInit_ex(digest_context, EVP_md4(), nullptr) != 1) {
-            EVP_MD_CTX_free(digest_context);
-            throw std::runtime_error("Failed to initialize digest context");
-        }
-    }
+    void initializeDigest(EVP_MD_CTX *digest_context) override;
 
 public:
     using AbstractHasher::AbstractHasher;
